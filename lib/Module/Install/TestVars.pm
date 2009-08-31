5bc370a866ac94aefdd0d5b22fe568c227561356
Toru Yamaguchi <trymch+public@gmail.com>
2009-08-31 07:23:43 +0000

    Import of ZIGOROU/Module-Install-TestVars-0.01_02 from CPAN.
    gitpan-cpan-distribution: Module-Install-TestVars
    gitpan-cpan-version:      0.01_02
    gitpan-cpan-path:         ZIGOROU/Module-Install-TestVars-0.01_02.tar.gz
    gitpan-cpan-author:       ZIGOROU
    gitpan-cpan-maturity:     developer

--- lib/Module/Install/TestVars.pm
+++ lib/Module/Install/TestVars.pm
@@ -3,43 +3,136 @@ package Module::Install::TestVars;
 use strict;
 use warnings;
 
-our $VERSION = '0.01_01';
+our $VERSION = '0.01_02';
 
 use base qw(Module::Install::Base);
 
+use Getopt::Long;
+use Term::ReadLine;
+use Term::UI;
+use Text::ASCIITable;
+
 sub test_vars {
-    my ($self, %vars) = @_;
-    %MY::TEST_VARS = ( %vars );
+    my ( $self, %config ) = @_;
+
+    my $is_help = 0;
+
+    GetOptions( 'help|h' => \$is_help, );
+
+    my $term = Term::ReadLine->new('test_vars');
+    my $out = $term->OUT || \*STDOUT;
+
+    if ($is_help) {
+        print $out 'Options: ', "\n\n";
+        for my $var ( keys %config ) {
+            $config{$var}->{prompt} ||= $var;
+
+            print $out sprintf(
+                "    %s : %s%s\n",
+                $var,
+                $config{$var}->{prompt},
+                (
+                    exists $config{$var}->{default}
+                    ? sprintf( " (default: %s)", $config{$var}->{default} )
+                    : ""
+                )
+            );
+        }
+        exit;
+    }
+
+ WIZARD:
+    my %test_vars = map { ( split( /=/, $_, 2 ) ) } @ARGV;
+    my $tbl = Text::ASCIITable->new( +{ headingText => 'test variables' } );
+    $tbl->setCols( 'key', 'value' );
+
+    print $out "Test configuration variables, \n\n";
+
+    for my $var ( keys %config ) {
+        unless ( exists $test_vars{$var} ) {
+            $config{$var}->{prompt} ||= 'Input ' . $var;
+
+            my %args = (
+                (exists $config{$var}->{default})
+                    ? (
+                        prompt  => $config{$var}{prompt},
+                        default => $config{$var}{default},
+                    ) : (
+                        prompt => $config{$var}{prompt},
+                    ),
+                ( exists $config{$var}->{choices} && ref $config{$var}->{choices} )
+                    ? (
+                        choices => $config{$var}{choices},
+                        print_me => 'Choise number'
+                    ) : (),
+            );
+
+            do {
+                $test_vars{$var} = $term->get_reply(%args);
+                $test_vars{$var} = $config{$var}{choices}->[$test_vars{$var} - 1] if (ref $config{$var}{choices});
+
+                
+            } until ( defined $test_vars{$var} );
+        }
+        else {
+            print $out sprintf( "%s: %s\n", $var, $test_vars{$var} );
+        }
+
+        $tbl->addRow( $var, $test_vars{$var} );
+    }
+
+    print $out "\n", $tbl, "\n";
+
+    unless (
+        $term->ask_yn(
+            prompt  => 'Check configuration, proceed?',
+            default => 'y'
+        )
+      )
+    {
+        goto WIZARD;
+    }
+
+    %MY::TEST_VARS = (%test_vars);
 }
 
 package MY;
 our %TEST_VARS = ();
+
 sub test {
     my ( $self, %attrs ) = @_;
-    my $vars = 'TEST_VARS=' . join(' ' => map { sprintf('%s=%s', $_, $TEST_VARS{$_}) } keys %TEST_VARS);
+    my $vars = 'TEST_VARS='
+      . join(
+        ' ' => map { sprintf( '%s=%s', $_, $TEST_VARS{$_} ) }
+          keys %TEST_VARS
+      );
     my $section = $self->SUPER::test(%attrs);
     $section = $vars . "\n" . $section;
-    $section =~ s|(PERL_DL_NONLAZY=1)|$1 \$\(TEST_VARS\)|g if (length $vars);
+    $section =~ s|(PERL_DL_NONLAZY=1)|$1 \$\(TEST_VARS\)|g if ( length $vars );
     $section;
 }
 
 1;
 __END__
 
-=for stopwords vars
+=for stopwords vars Makefile.PL
 
 =head1 NAME
 
-Module::Install::TestVars - Passing variables before running tests
+Module::Install::TestVars - Setting and Passing test variables.
 
 =head1 SYNOPSIS
+
+In your Makefile.PL, 
 
   use inc::Module::Install;
 
   test_vars
-    TEST_DBI_DSN      => 'dbi:mysql:dbname=test',
-    TEST_DBI_USER     => 'root',
-    TEST_DBI_PASSWORD => '',
+    TEST_DBI_DSN      => +{ default => 'dbi:mysql:dbname=test', prompt => 'input DBI DSN param' },
+    TEST_DBI_USER     => +{ default => 'root', prompt => 'input DBI USER param', },
+    TEST_DBI_PASSWORD => +{ default => '', prompt => 'input DBI PASSWORD param', };
+
+Running Makefile.PL, asking values and passing variables to tests.
 
 In your test,
 
@@ -53,13 +146,14 @@ In your test,
 
 =head1 DESCRIPTION
 
-Module::Install::TestVars is module fetching test setting variables via %ENV vars.
+Module::Install::TestVars is module asking and setting test variable.
+Please see unit tests for usage details.
 
 =head1 METHODS
 
 =head2 test_vars %vars
 
-key-value.
+key-setting.
 
 =head1 AUTHOR
 
